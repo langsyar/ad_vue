e998a720cba75d26e4eb631a5eb1a75b2b46e41b
Langsyar <langsyar@gmail.com>
2019-01-28 01:57:30 +0300

    vue-router and toolbar

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
-import './plugins/vuetify'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import router from './router'
 
-Vue.config.productionTip = false
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
